77926b152b4b12b2543873d220be4f4712ec1929
Bhawin Dhital <bhawindhital@Bhawin.local>
2019-12-01 16:16:50 -0500

    p1.py

---
+++ p1.py
@@ -0,0 +1,28 @@
+#Python learning first step: Beginner's guide:
+# This followsVariables and strings
+x = 5 # x is a variable and 5 is stored in x
+country = 'Nepal'
+print(country)
+print("Hello world")
+say="Hello world"
+print(say)
+print(type(say))
+x = 5
+print(type(x))
+# Combining two string
+
+First_name = 'Hem'
+Last_name = 'Dutt'
+Full_name = First_name +' ' + Last_name # If you do not put ' ' in between first and last name then you get HemDutt.
+print(Full_name)
+
+#Indexing of an element in a string
+"abcde".index("a")
+
+#Examples of List:
+Friends = ['Hem', 'Thir', 'Bishnu']
+First_friend=Friends[0]
+print(First_friend)
+name = "Hem Dutt"
+print(name.upper())
+print(name.lower())
